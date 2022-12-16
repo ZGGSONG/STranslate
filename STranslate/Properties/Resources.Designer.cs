0287e8cd62f9fcb33a53c6e749467e996874a384
ZGGSONG <zggsong@foxmail.com>
2022-12-16 17:25:55 +0800

    feat: add notify

--- STranslate/Properties/Resources.Designer.cs
+++ STranslate/Properties/Resources.Designer.cs
@@ -119,5 +119,15 @@ namespace STranslate.Properties {
                 return ((byte[])(obj));
             }
         }
+        
+        /// <summary>
+        ///   查找 System.Byte[] 类型的本地化资源。
+        /// </summary>
+        internal static byte[] translate2 {
+            get {
+                object obj = ResourceManager.GetObject("translate2", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
     }
 }
