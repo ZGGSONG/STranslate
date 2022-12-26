ccdad1fb0487b60abfcd533c6a94d8b5e0c08adc
DESKTOP-3BO4HSG\ksat <zggsong@foxmail.com>
2022-12-26 16:47:21 +0800

    perf: update ui and stringconverter by original code

---
+++ STranslate/Model/VisibilityConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace STranslate.Model
+{
+    public class VisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return string.IsNullOrEmpty((string)value) ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value != null && (Visibility)value == Visibility.Collapsed;
+        }
+    }
+}
