711088201e64952a12217b0040d94b283099572d
zggsong <zggsong@foxmail.com>
2023-06-26 22:32:58 +0800

    fix some bugs

--- STranslate.Settings/View/MainWindow.xaml.cs
+++ STranslate.Settings/View/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace STranslate.Settings
+namespace STranslate.Settings.View
 {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
