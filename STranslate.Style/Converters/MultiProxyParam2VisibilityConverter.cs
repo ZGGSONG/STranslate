db09e3303ef8f70bf9372a4f51b90907be4f89f2
zggsong <zggsong@foxmail.com>
2024-02-29 16:26:38 +0800

    perf: Optimizing the configuration page

---
+++ STranslate.Style/Converters/MultiProxyParam2VisibilityConverter.cs
@@ -0,0 +1,45 @@
+﻿using STranslate.Model;
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace STranslate.Style.Converters
+{
+    /// <summary>
+    /// 多条数据确定显示隐藏
+    /// </summary>
+    internal class MultiProxyParam2VisibilityConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                var proxyMethod = (ProxyMethodEnum)values[0];
+                var isAuth = (bool)values[1];
+
+                var proxyVisibility = proxyMethod switch
+                {
+                    ProxyMethodEnum.不使用代理 => Visibility.Collapsed,
+                    ProxyMethodEnum.系统代理 => Visibility.Collapsed,
+                    ProxyMethodEnum.HTTP => Visibility.Visible,
+                    ProxyMethodEnum.SOCKS5 => Visibility.Visible,
+                    _ => Visibility.Collapsed
+                };
+
+                var authVisibility = isAuth ? Visibility.Visible : Visibility.Collapsed;
+
+                return proxyVisibility | authVisibility;
+            }
+            catch (Exception)
+            {
+                return Visibility.Collapsed;
+            }
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
