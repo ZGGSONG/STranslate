934b81e302955ac75dab70e7b460ff4804c25f25
zggsong <zggsong@foxmail.com>
2024-03-06 17:38:03 +0800

    perf: enhance the display effect of the service list.

--- STranslate/Views/Preference/ServicePage.xaml.cs
+++ STranslate/Views/Preference/ServicePage.xaml.cs
@@ -89,7 +89,8 @@ namespace STranslate.Views.Preference
             return toggleButton != null && item.IsAncestorOf(toggleButton);
         }
 
-        public static T? FindAncestor<T>(DependencyObject current) where T : DependencyObject
+        public static T? FindAncestor<T>(DependencyObject current)
+            where T : DependencyObject
         {
             do
             {
@@ -98,9 +99,28 @@ namespace STranslate.Views.Preference
                     return t;
                 }
                 current = VisualTreeHelper.GetParent(current);
-            }
-            while (current != null);
+            } while (current != null);
             return null;
+        }
+
+        /// <summary>
+        /// ListBox鼠标滚轮事件处理函数
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ServiceListBox_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            //按住Ctrl滚动时不将滚动冒泡给上一层级的控件
+            if (!e.Handled && !Keyboard.IsKeyDown(Key.LeftCtrl) && !Keyboard.IsKeyDown(Key.RightCtrl))
+            {
+                // ListBox拦截鼠标滚轮事件
+                e.Handled = true;
+
+                // 激发一个鼠标滚轮事件，冒泡给外层ListBox接收到
+                var eventArg = new MouseWheelEventArgs(e.MouseDevice, e.Timestamp, e.Delta) { RoutedEvent = MouseWheelEvent, Source = sender };
+                var parent = ((Control)sender).Parent as UIElement;
+                parent!.RaiseEvent(eventArg);
+            }
         }
     }
 }
