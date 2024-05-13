292e2f299ffa53719789e452ded9be0a0bdaf917
zggsong <zggsong@foxmail.com>
2024-05-13 21:18:15 +0800

    perf: 加回软件自带更新功能，优化更新

---
+++ src/STranslate.Updater/MainViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace STranslate.Updater
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private double _processValue;
+
+        public double ProcessValue
+        {
+            get => _processValue;
+            set => UpdateProperty(ref _processValue, value);
+        }
+
+        private string _version = "";
+
+        public string Version
+        {
+            get => _version;
+            set => UpdateProperty(ref _version, value);
+        }
+
+        public MainViewModel() { }
+
+        public MainViewModel(string version)
+        {
+            Version = version;
+        }
+
+        #region PropertyChanged
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void UpdateProperty<T>(ref T properValue, T newValue, [CallerMemberName] string properName = "")
+        {
+            if (Equals(properValue, newValue))
+                return;
+            properValue = newValue;
+            NotifyPropertyChanged(properName);
+        }
+
+        public void NotifyPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion PropertyChanged
+    }
+}
--- src/STranslate/ViewModels/Preference/AboutViewModel.cs
+++ src/STranslate/ViewModels/Preference/AboutViewModel.cs
@@ -1,15 +1,17 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Threading;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 using STranslate.Log;
 using STranslate.Model;
 using STranslate.Style.Controls;
 using STranslate.Util;
+using System.Threading;
 
 namespace STranslate.ViewModels.Preference
 {
@@ -26,22 +28,64 @@ namespace STranslate.ViewModels.Preference
         [RelayCommand]
         private void OpenLink(string url) => Process.Start(new ProcessStartInfo { FileName = url, UseShellExecute = true });
 
+        [ObservableProperty]
+        private bool _isChecking = false;
+
         [RelayCommand(IncludeCancelCommand = true)]
         private async Task CheckUpdateAsync(CancellationToken token)
         {
             try
             {
-                var resp = await HttpUtil.GetAsync(ConstStr.GITHUBRELEASEURL, token);
-                var parseData = JsonConvert.DeserializeObject<JObject>(resp);
-                var remoteVer = parseData?["tag_name"]?.ToString() ?? ConstStr.DEFAULTVERSION;
-                var canUpdate = StringUtil.IsCanUpdate(remoteVer, Version);
-                MessageBox_S.Show(canUpdate ? $"检测到最新版本: {remoteVer}\n当前版本: {Version}" : $"恭喜您, 当前为最新版本!");
+                const string updateFolder = "Update";
+
+                string GetPath(string fileName) => Path.Combine(AppDomain.CurrentDomain.BaseDirectory, fileName);
+
+                string GetCachePath(string fileName) => Path.Combine(AppDomain.CurrentDomain.BaseDirectory, updateFolder, fileName);
+
+                string[] requiredFiles = ["Updater.exe", "Updater.dll", "Newtonsoft.Json.dll", "Updater.deps.json", "Updater.runtimeconfig.json"];
+
+                if (!Directory.Exists(GetPath(updateFolder)))
+                {
+                    Directory.CreateDirectory(GetPath(updateFolder));
+                }
+
+                if (requiredFiles.All(file => File.Exists(GetPath(file))))
+                {
+                    foreach (var file in requiredFiles)
+                    {
+                        File.Copy(GetPath(file), GetCachePath(file), true);
+                    }
+
+                    CommonUtil.ExecuteProgram(GetCachePath("Updater.exe"), [Version]);
+                }
+                else
+                {
+                    throw new Exception("升级程序似乎遭到破坏，请手动前往发布页查看新版本");
+                }
             }
-            catch (OperationCanceledException) { }
             catch (Exception ex)
             {
-                MessageBox_S.Show($"检查更新出错, 请检查网络情况");
-                LogService.Logger.Warn($"检查更新出错, 请检查网络情况, {ex.Message}");
+                try
+                {
+                    IsChecking = true;
+                    var resp = await HttpUtil.GetAsync(ConstStr.GITHUBRELEASEURL, token);
+                    var parseData = JsonConvert.DeserializeObject<JObject>(resp);
+                    var remoteVer = parseData?["tag_name"]?.ToString() ?? ConstStr.DEFAULTVERSION;
+                    var canUpdate = StringUtil.IsCanUpdate(remoteVer, Version);
+                    MessageBox_S.Show(canUpdate ? $"检测到最新版本: {remoteVer}\n当前版本: {Version}" : $"恭喜您, 当前为最新版本!");
+                }
+                catch (OperationCanceledException) { }
+                catch (Exception e)
+                {
+                    MessageBox_S.Show($"检查更新出错, 请检查网络情况");
+                    LogService.Logger.Warn($"检查更新出错, 请检查网络情况, {e.Message}");
+                }
+                finally
+                {
+                    IsChecking = false;
+                }
+
+                LogService.Logger.Warn($"更新程序已打开或无法正确启动检查更新程序, {ex.Message}");
             }
         }
     }
