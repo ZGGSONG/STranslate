d29e96eca2e8e45b366ff1e03f701498e5d06ef0
zggsong <zggsong@foxmail.com>
2024-08-09 17:58:02 +0800

    chore: update minimal btn style

--- src/STranslate/ViewModels/Preference/CommonViewModel.cs
+++ src/STranslate/ViewModels/Preference/CommonViewModel.cs
@@ -6,7 +6,6 @@ using Newtonsoft.Json;
 using STranslate.Helper;
 using STranslate.Log;
 using STranslate.Model;
-using STranslate.Style.Controls;
 using STranslate.Util;
 using STranslate.Views;
 using STranslate.Views.Preference;
