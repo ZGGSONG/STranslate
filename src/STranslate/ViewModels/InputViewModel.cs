fd823b0d48453090588b4ed4c14c4826db9cf3da
orzuemu <orzuemu@gmail.com>
2024-09-21 16:11:32 +0800

    feat: 适配deepseek服务

--- src/STranslate/ViewModels/InputViewModel.cs
+++ src/STranslate/ViewModels/InputViewModel.cs
@@ -685,6 +685,7 @@ public class CurrentTranslatorConverter : JsonConverter<ITranslator>
                 (int)ServiceType.DeepLService => new TranslatorDeepL(),
                 (int)ServiceType.AzureOpenAIService => new TranslatorAzureOpenAI(),
                 (int)ServiceType.ClaudeService => new TranslatorClaude(),
+                (int)ServiceType.DeepSeekService => new TranslatorDeepSeek(),
                 //TODO: 新接口需要适配
                 _ => new TranslatorApi()
             };
