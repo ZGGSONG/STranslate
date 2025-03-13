9b4e0d7c42ed243d7adefb997e54994e226b0bb0
zggsong <zggsong@foxmail.com>
2025-03-13 17:09:06 +0800

    perf: 移除网络端口控件数字限制&焦点消失时进行值转换

--- src/STranslate/Views/Preference/CommonPage.xaml.cs
+++ src/STranslate/Views/Preference/CommonPage.xaml.cs
@@ -18,20 +18,4 @@ public partial class CommonPage : UserControl
         Singleton<CommonViewModel>.Instance.OnOftenUsedLang +=
             () => BindingOperations.GetMultiBindingExpression(LangCb, ItemsControl.ItemsSourceProperty)?.UpdateTarget();
     }
-
-    /// <summary>
-    ///     限制输入数字
-    /// </summary>
-    /// <param name="sender"></param>
-    /// <param name="e"></param>
-    private void AcceptOnlyNumber(object sender, KeyEventArgs e)
-    {
-        if (!IsAllowedKey(e.Key)) e.Handled = true;
-    }
-
-    private bool IsAllowedKey(Key key)
-    {
-        return (key >= Key.D0 && key <= Key.D9) || (key >= Key.NumPad0 && key <= Key.NumPad9) || key == Key.Back ||
-               key == Key.Delete || key == Key.Left || key == Key.Right;
-    }
 }
