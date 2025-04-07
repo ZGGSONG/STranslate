5e46ba916deb280cbae8a884edc7246f378637d2
zggsong <zggsong@foxmail.com>
2025-04-07 21:33:25 +0800

    chore: location

--- src/STranslate/ViewModels/SsTranslateViewModel.cs
+++ src/STranslate/ViewModels/SsTranslateViewModel.cs
@@ -8,6 +8,7 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
 namespace STranslate.ViewModels;
@@ -32,6 +33,14 @@ public partial class SsTranslateViewModel : ObservableObject
     {
         SsTranslateBs = BitmapUtil.ConvertBitmap2BitmapSource(bs, ImageFormat.Png);
         var view = new SsTranslateView(this);
+
+        var dpi = VisualTreeHelper.GetDpi(view);
+
+        // 获取鼠标位置并设置窗口位置为鼠标左上角位置
+        var mousePosition = CommonUtil.GetPositionInfos().Item1;
+        view.Left = mousePosition.X - bs.Size.Width / dpi.DpiScaleX;
+        view.Top = mousePosition.Y - bs.Size.Height / dpi.DpiScaleY;
+        
         view.Show();
 
         bs.Dispose();
