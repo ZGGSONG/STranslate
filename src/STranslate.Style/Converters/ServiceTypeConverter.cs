82dc2c8c42ba614a46fb318eb617d6af636c9f1d
zggsong <zggsong@foxmail.com>
2025-05-22 14:57:47 +0800

    支持i18n，但是不能动态更新

---
+++ src/STranslate.Style/Converters/ServiceTypeConverter.cs
@@ -0,0 +1,26 @@
+﻿using STranslate.Model;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace STranslate.Style.Converters;
+
+public class ServiceTypeConverter : MarkupExtension, IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is not ServiceType st) return value;
+
+        return AppLanguageManager.GetString($"ServiceType.{st}");
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return Binding.DoNothing;
+    }
+
+    public override object ProvideValue(IServiceProvider serviceProvider)
+    {
+        return this;
+    }
+}
