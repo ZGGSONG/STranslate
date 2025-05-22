0db65274aca5d97f1e74d5d35a5f21d4afb92f18
zggsong <zggsong@foxmail.com>
2025-05-22 15:03:32 +0800

    perf: fixed string after removing translation service type

---
+++ src/STranslate.Style/Converters/ServiceTypeConverter.cs
@@ -0,0 +1,27 @@
+﻿using STranslate.Model;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace STranslate.Style.Converters;
+
+public class ServiceTypeConverter : MarkupExtension, IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is not ServiceType st) return value;
+
+        // 移除最后的Service
+        return st.ToString().Replace("Service", string.Empty);
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return Binding.DoNothing;
+    }
+
+    public override object ProvideValue(IServiceProvider serviceProvider)
+    {
+        return this;
+    }
+}
